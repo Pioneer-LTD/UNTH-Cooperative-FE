037a4abc6f911c11319597ce1c9f892c89e7a333
theseyipeters <theseyipeters@gmail.com>
2023-08-23 09:07:10 +0100

    signUp page and forgotPassword

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
-// import { useState } from 'react'
-import { Route,Routes } from 'react-router-dom'
-import './App.css'
-import Login from './pages/login/Login'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Login from "./pages/login/Login";
+import SignUp from "./pages/signUp/SignUp";
+import PasswordReset from "./pages/password/PasswordReset";
 
 function App() {
-
-  return (
-    <>
-    <Routes>
-      <Route path='/' element={<Login/>}/>
-    </Routes>
-    </>
-  )
+	return (
+		<>
+			<Routes>
+				<Route
+					path="/"
+					element={<Login />}
+				/>
+				<Route
+					path="/signup"
+					element={<SignUp />}
+				/>
+				<Route
+					path="/resetpassword"
+					element={<PasswordReset />}
+				/>
+			</Routes>
+		</>
+	);
 }
 
-export default App
+export default App;
