aa74e19e250da2319a00e2c3729baf6bf1791d25
onyekachii <emegwaonyeka@gmail.com>
2023-08-23 10:46:58 +0100

    final authpages layout

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,25 +1,29 @@
-
 import PropTypes from "prop-types";
 
-const Input = ({type,label,placeHolder}) => {
-  return (
-    <div className="flex flex-col text-start">
-      <label htmlFor="" className='text-sm mb-1'>{label + '*'}</label>
-      <input
-        type={type}
-        placeholder={placeHolder}
-        className="border border-slate-400 outline-none rounded p-2 mb-2"
-      />
-    </div>
-  );
-}
-
-
-Input.propTypes = {
-  type: PropTypes.string.isRequired,
-  label: PropTypes.string.isRequired,
-  placeHolder: PropTypes.string.isRequired,
+const Input = ({ type, label, placeHolder, onChange, value }) => {
+	return (
+		<div className="flex flex-col text-start">
+			<label
+				htmlFor=""
+				className="text-sm mb-1">
+				{label}
+				<span className="reqired text-red-500 mx-1">*</span>
+			</label>
+			<input
+				value={value}
+				type={type}
+				placeholder={placeHolder}
+				onChange={onChange}
+				className="border border-slate-400 outline-none rounded p-2 mb-2"
+			/>
+		</div>
+	);
 };
 
+Input.propTypes = {
+	type: PropTypes.string.isRequired,
+	label: PropTypes.string.isRequired,
+	placeHolder: PropTypes.string.isRequired,
+};
 
-export default Input
+export default Input;
